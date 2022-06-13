246450291a874c16cde425405384d6537cf7229a
David.Gao <david.alpha.fox@gmail.com>
2022-06-13 17:27:26 +0800

    optimize EMA

--- src/routes_example/utils/ma.cljs
+++ src/routes_example/utils/ma.cljs
@@ -3,20 +3,20 @@
 (defn  moving-average 
   "简单移动平均线"
   [period coll]
-  (lazy-cat (repeat (dec period) nil)
-            (map #(/ (reduce + %) period))))
+  (vec (lazy-cat (repeat (dec period) nil)
+            (map #(/ (reduce + %) period)))))
 (defn exponential-moving-average
   "指数移动平均值"
   [period coll]
   (let [k (/ 2 (+ period 1))
-        p (- 1 k)]
-    (reduce (fn [acc item]
-              (let [ema-last (last acc)
-                    ema-now (if (first acc)
-                             (+ (* k item) (* p ema-last))
-                              item)]
-                (conj acc ema-now)))
-            [] coll)))
+        p (- 1 k)
+        x1 (first coll)
+        n (count coll)]
+    (loop [i 1 ema-last x1 acc (transient [x1])]
+      (if (< i n)
+        (let [ema-now (+ (* k (nth coll i)) (* p ema-last))]
+          (recur (+ i 1) ema-now (conj! acc ema-now)))
+        (persistent! acc)))))
 ;;(def data '(10.94 12.03 13.23 14.55 16.01))
 ;;period 5
 ;;(def result '(10.940 11.303 11.946 12.814 13.879))
